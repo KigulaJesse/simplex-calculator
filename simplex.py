ca0829ac232a5493184bb679c1d7737e3083354c
KigulaJesse <jkigula@icloud.com>
2021-06-12 21:16:30 +0300

    changes

--- simplex.py
+++ simplex.py
@@ -72,150 +72,160 @@ def is_optimal(tableau):
 #          is a list of the tableau excluding 
 #          the row which has the objective function         
 def determine_pivot_row(optimal, st_rows_minus_maximise):
-          smallest_division = 1000000000
-          z = 0
-          for i in st_rows_minus_maximise:
-                    value = i[-1]/i[optimal]
-                    if(value < smallest_division):
-                              smallest_division = value
-                              chosen_index = z
+          try:
+                    smallest_division = 1000000000
+                    z = 0
+                    for i in st_rows_minus_maximise:
+                              value = i[-1]/i[optimal]
+                              if(value < smallest_division):
+                                        smallest_division = value
+                                        chosen_index = z
 
-                    z += 1
-          return chosen_index
+                              z += 1
+                    return chosen_index
+          except:
+                    print("\n*****WRONG INPUT*****")
+                    print("     Check your values   \n")
+                    
 
 #This is the Simplex tableau function that takes a tableau as a parameter              
 def Simplex(tableau, w):
-          #new tableau is where the new tableau at end of this function will be stored
-          #start by copying and pasting the tableau in new tableau
-          new_tableau = tableau.copy()
-          tableau_print = [['x1','x2','x3']]
-          t = 0
-          m = 1
-          for i in tableau[0]:
-                    if t <= 2:
-                              t += 1
-                              continue
+          try:
+                    #new tableau is where the new tableau at end of this function will be stored
+                    #start by copying and pasting the tableau in new tableau
+                    new_tableau = tableau.copy()
+                    tableau_print = [['x1','x2','x3']]
+                    t = 0
+                    m = 1
+                    for i in tableau[0]:
+                              if t <= 2:
+                                        t += 1
+                                        continue
+                              else:
+                                        s_column = "s" + str(m)
+                                        t += 1
+                                        m += 1
+                                        tableau_print[0].append(s_column)
+
+                                        if t == (len(tableau[0]) - 2):
+                                                  break
+                    tableau_print[0].append("z")
+                    tableau_print[0].append("b")
+                    
+                    #call the is_optimal function.
+                    #EITHER 
+                    #         index position e.g (0, 1, 2) of column 
+                    #         which has the least negative value
+                    #         (index of a list position can never be a negative ) 
+                    #OR 
+                    #         -1 is returned and stored in the optimal variable
+                    optimal = is_optimal(tableau)
+
+                    #if optimal == -1, the solution had no negatives in its objective function
+                    # it is optimal 
+                    if optimal == -1:
+                              cols = []
+                              for x in range(0,len(tableau[0]),1):
+                                        cols.append([i[x] for i in tableau])
+
+                              solution = []
+
+                              z = 0
+                              for i in cols:
+                                        if z == (len(cols)-1):
+                                                  break
+                                        y = 0
+                                        count_0s = 0
+                                        count_1s = 0
+                                        for p in i:
+                                                  
+                                                  if p == 1:
+                                                            col = y
+                                                            count_1s += 1
+                                                  if p == 0:
+                                                            count_0s += 1
+                                                  y += 1
+
+                                        z += 1
+
+                                        if (count_0s == (len(i) - 1)) & (count_1s == 1):
+                                                  solution.append(cols[-1][col])
+                                        else:
+                                                  solution.append(0)
+
+                              print("\nThe solution is optimal when")
+                              g = 1
+                              k = 1
+                              for f in solution:
+                                        if g <= 3:          
+                                                  print("ci"+str(g)+" = " + str(f))
+                                                  g += 1
+                                        elif g < len(solution):
+                                                  print("s"+str(k)+" = " + str(f))
+                                                  k += 1
+                                                  g += 1
+                                        else:
+                                                  print("z = " + str(f))
+
+                              return -1
+                    #else
                     else:
-                              s_column = "s" + str(m)
-                              t += 1
-                              m += 1
-                              tableau_print[0].append(s_column)
+                              #a list of the pivot column stored in smallest_column
+                              smallest_column = [i[optimal] for i in tableau]
 
-                              if t == (len(tableau[0]) - 2):
-                                        break
-          tableau_print[0].append("z")
-          tableau_print[0].append("b")
-           
-          #call the is_optimal function.
-          #EITHER 
-          #         index position e.g (0, 1, 2) of column 
-          #         which has the least negative value
-          #         (index of a list position can never be a negative ) 
-          #OR 
-          #         -1 is returned and stored in the optimal variable
-          optimal = is_optimal(tableau)
+                              st_rows_minus_maximise = []
+                              for i in tableau:
+                                        if(i == tableau[-1]):
+                                                  continue
+                                        st_rows_minus_maximise.append(i) 
+                              chosen_row = determine_pivot_row(optimal,st_rows_minus_maximise)
+                              smallest_value = tableau[chosen_row][optimal]
 
-          #if optimal == -1, the solution had no negatives in its objective function
-          # it is optimal 
-          if optimal == -1:
-                    cols = []
-                    for x in range(0,len(tableau[0]),1):
-                              cols.append([i[x] for i in tableau])
-
-                    solution = []
-
-                    z = 0
-                    for i in cols:
-                              if z == (len(cols)-1):
-                                        break
-                              y = 0
-                              count_0s = 0
-                              count_1s = 0
-                              for p in i:
-                                        
-                                        if p == 1:
-                                                  col = y
-                                                  count_1s += 1
-                                        if p == 0:
-                                                  count_0s += 1
-                                        y += 1
-
-                              z += 1
-
-                              if (count_0s == (len(i) - 1)) & (count_1s == 1):
-                                        solution.append(cols[-1][col])
-                              else:
-                                        solution.append(0)
-
-                    print("\nThe solution is optimal when")
-                    g = 1
-                    k = 1
-                    for f in solution:
-                              if g <= 3:          
-                                        print("ci"+str(g)+" = " + str(f))
-                                        g += 1
-                              elif g < len(solution):
-                                        print("s"+str(k)+" = " + str(f))
-                                        k += 1
-                                        g += 1
-                              else:
-                                        print("z = " + str(f))
-
-                    return -1
-          #else
-          else:
-                    #a list of the pivot column stored in smallest_column
-                    smallest_column = [i[optimal] for i in tableau]
-
-                    st_rows_minus_maximise = []
-                    for i in tableau:
-                              if(i == tableau[-1]):
-                                        continue
-                              st_rows_minus_maximise.append(i) 
-                    chosen_row = determine_pivot_row(optimal,st_rows_minus_maximise)
-                    smallest_value = tableau[chosen_row][optimal]
-
-                    print("=======================================")
-                    print("pivot column = " + str(smallest_column))
-                    print("pivot row = " + str(tableau[chosen_row]))
+                              print("=======================================")
+                              print("pivot column = " + str(smallest_column))
+                              print("pivot row = " + str(tableau[chosen_row]))
+                              print("pivot value = " + str(smallest_value))
 
 
-                    #----FORMULATING THE NEW TABLEAU------------#
-                    
-                    z = 0
-                    for i in new_tableau[chosen_row]:
-                              i = i / smallest_value
-                              new_tableau[chosen_row][z] = i
-                              z += 1
-                    
-                    z = 0
-                    for i in new_tableau:
-                              if (z == chosen_row):
+                              #----FORMULATING THE NEW TABLEAU------------#
+                              
+                              z = 0
+                              for i in new_tableau[chosen_row]:
+                                        i = i / smallest_value
+                                        new_tableau[chosen_row][z] = i
+                                        z += 1
+                              
+                              z = 0
+                              for i in new_tableau:
+                                        if (z == chosen_row):
+                                                  z = z + 1
+                                                  continue
+                                        x = 0
+                                        for k in new_tableau[z]:
+                                                  new_tableau[z][x] = new_tableau_value_calc(smallest_column[z], new_tableau[chosen_row][x],tableau[z][x])
+                                                  x += 1 
                                         z = z + 1
-                                        continue
-                              x = 0
-                              for k in new_tableau[z]:
-                                        new_tableau[z][x] = new_tableau_value_calc(smallest_column[z], new_tableau[chosen_row][x],tableau[z][x])
-                                        x += 1 
-                              z = z + 1
 
 
-                    
-                    print("\n")
-                    print("tableau[" + str(w) + "]")
-                    for i in new_tableau:
-                              tableau_print.append(i)
-                    
-                    s = [[str(e) for e in row] for row in tableau_print]
-                    lens = [max(map(len, col)) for col in zip(*s)]
-                    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-                    table = [fmt.format(*row) for row in s]
-                    print ("\n".join(table))
+                              
+                              print("\n")
+                              print("tableau[" + str(w) + "]")
+                              for i in new_tableau:
+                                        tableau_print.append(i)
+                              
+                              s = [[str(e) for e in row] for row in tableau_print]
+                              lens = [max(map(len, col)) for col in zip(*s)]
+                              fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+                              table = [fmt.format(*row) for row in s]
+                              print ("\n".join(table))
 
+                              
+                              
+                              return 1
+          except:
+                    print("\n*****WRONG INPUT*****")
+                    print("     Check your values   \n")
                     
-                    
-                    return 1
-          
 
 tableau = []
 
